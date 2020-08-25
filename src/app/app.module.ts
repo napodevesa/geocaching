7c625c05d617150574cd234ad994f8adefe2a0b2
ndev <napodevesa@gmail.com>
2020-08-25 13:48:41 +0200

    LugarPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {LugarPage} from '../pages/lugar/lugar';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
+  declarations:
+  [MyApp, HomePage, LugarPage],
+
+   [AppComponent],
+  entryComponents: [MyApp, HomePage, LugarPage],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
